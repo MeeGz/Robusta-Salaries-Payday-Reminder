daf8d121d237bf806887af20d289003be38a5c84
meegz <mohamedmagdyzakareiia@gmail.com>
2019-07-24 01:22:37 +0000

    get all stored payments - get all payments for this year

--- app/Http/Controllers/API_V1_0/PaymentController.php
+++ app/Http/Controllers/API_V1_0/PaymentController.php
@@ -9,16 +9,27 @@ use Carbon\Carbon;
 class PaymentController extends Controller
 {
     /**
+     * Display a listing of the resource for this year.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function yearPayments()
+    {
+        $now = Carbon::now();
+        $model = new Payment;
+        $payments = Payment::where('year', $now->year)->select("month", "salaries_total", "salaries_payment_day", "bonus_total", "bonus_payment_day")->get();
+        $payments = $model->getRemainderMonths($payments, $now);
+        return response()->json($payments, 200);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $now = Carbon::now();
-        $model = new Payment;
-        $payments = Payment::where('year', $now->year)->select("month", "salaries_total", "salaries_payment_day", "bonus_total", "bonus_payment_day")->get();
-        $payments = $model->getRemainderMonths($payments, $now);
+        $payments = Payment::select("month", "salaries_total", "salaries_payment_day", "bonus_total", "bonus_payment_day")->get();
         return response()->json($payments, 200);
     }
 }
