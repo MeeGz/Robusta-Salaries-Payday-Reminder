25757e02a80cfd23bf56f5d4a3f3cd854db2c830
meegz <mohamedmagdyzakareiia@gmail.com>
2019-07-27 08:57:53 +0000

    code cleaning - updating reminder contract

--- app/Reminder/ReminderInterface.php
+++ app/Reminder/ReminderInterface.php
@@ -5,8 +5,4 @@ namespace App\Reminder;
 interface ReminderInterface
 {
     public function handle(): void;
-    public function setReminder(int $day): void;
-    public function willSend($day): bool;
-    public function getAdminsEmails(): array;
-    public function sendEmails(array $emails, ReminderInterface $reminder): void;
 }
